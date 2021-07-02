e3a73fdd30b09f32bea2cad7db4f3ac6b5c0bd9c
György Császár <csaszar.gyorgy890322@gmail.com>
2021-07-02 23:36:11 +0200

    add search possibilities to MovieStore web app

--- MovieStoreWebApp/Pages/Movies/Index.cshtml.cs
+++ MovieStoreWebApp/Pages/Movies/Index.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MovieStoreWebApp.Models;
+using System.Linq;
 
 namespace MovieStoreWebApp.Pages.Movies
 {
@@ -15,11 +18,32 @@ namespace MovieStoreWebApp.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var genreQuery = _context.Movie.OrderBy(m => m.Genre).Select(m => m.Genre);
+            var movies = _context.Movie.Select(m => m);
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(m => m.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(m => m.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
