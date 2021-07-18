dbffe0eba5d36c54e7b794e22c33fe5906ca4fb3
György Császár <csaszar.gyorgy890322@gmail.com>
2021-07-18 22:00:23 +0200

    Add new data annotations to the movie model

--- MovieStoreWebApp/Migrations/20210718195801_New_DataAnnotations.Designer.cs
+++ MovieStoreWebApp/Migrations/20210718195801_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieStoreWebApp.Data;
 
 namespace MovieStoreWebApp.Migrations
 {
     [DbContext(typeof(MovieStoreWebAppContext))]
-    partial class MovieStoreWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20210718195801_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
